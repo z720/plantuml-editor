167d816b3a528248533b52c01a39f4f7b213dc0a
z720 <seb@z720.net>
2022-03-21 10:46:58 +0100

    Prepare for Github Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false
+		}),
+		paths: {
+			base: dev ? '' : '/plantuml-editor',
+		},
+		trailingSlash: 'always',
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
 	}
 };
 
