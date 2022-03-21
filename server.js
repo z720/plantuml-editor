7ae8654b41c239450b12c62bf80bcd37e6247f38
z720 <seb@z720.net>
2022-03-21 20:50:11 +0100

    Updates

--- server.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static(__dirname));
-
-app.listen(process.env.PORT || 3000);
-console.log("Server started on port " + process.env.PORT || 3000);
