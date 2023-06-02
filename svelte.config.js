11e978a6d37c3d7f8c3409d1d359598303d39155
z720 <seb@z720.net>
2023-06-02 14:08:51 +0200

    update: bump dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -6,20 +6,22 @@ const ghpages = process.env.TARGET === 'github-pages';
 const config = {
 	kit: {
 		adapter: adapter({
-			// default options are shown
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
 			fallback: 'index.html',
-			precompress: false
+			precompress: false,
+			strict: true
 		}),
 		paths: {
 			base: ghpages ? '/plantuml-editor' : '',
 		},
-		trailingSlash: 'always',
-		prerender: {
-			// This can be false if you're using a fallback (i.e. SPA mode)
-			default: true
-		}
+		// trailingSlash: 'always',
+		// prerender: {
+		// 	// This can be false if you're using a fallback (i.e. SPA mode)
+		// 	default: true
+		// }
 	}
 };
 
